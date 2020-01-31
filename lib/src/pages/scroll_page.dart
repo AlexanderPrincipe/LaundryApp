eba1a423cd5443d3afaae0923d17d0ba87f30f59
Alexander <principetolentinoa@hotmail.com>
2020-01-30 23:37:51 -0500

    iconsadd

--- lib/src/pages/scroll_page.dart
+++ lib/src/pages/scroll_page.dart
@@ -3,8 +3,13 @@ import 'dart:math';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class ScrollPage extends StatelessWidget {
+
+    final estiloTitulo = TextStyle( fontSize: 17.0, fontWeight: FontWeight.bold );
+    final estilosubTitulo = TextStyle( fontSize: 15.0, color: Colors.grey );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,7 +17,7 @@ class ScrollPage extends StatelessWidget {
         scrollDirection: Axis.vertical,
         children: <Widget>[
           _pagina1(),
-          _pagina2(),
+          _pagina2(context),
           _pagina3(context)
 
 
@@ -25,15 +30,17 @@ class ScrollPage extends StatelessWidget {
   Widget _pagina1(){
     return Stack(
       children: <Widget>[
-        _colorFondo(),
-        _imagenFondo(),
+        //_colorFondo(),
+        //_imagenFondo(),
+        _fondoApp2(),
         _textos(),
 
       ],
     );
   }
 
-  Widget _colorFondo() {
+
+  /*Widget _colorFondo() {
     return Container(
       width: double.infinity,
       height: double.infinity,
@@ -41,7 +48,7 @@ class ScrollPage extends StatelessWidget {
     );
   }
 
-  Widget _imagenFondo(){
+   Widget _imagenFondo(){
     return Container(
       width: double.infinity,
       height: double.infinity,
@@ -50,6 +57,101 @@ class ScrollPage extends StatelessWidget {
         fit: BoxFit.cover,
       ),
     );
+  }
+  */
+
+   Widget _fondoApp2(){
+
+    final gradiente = Container(
+      width: double.infinity,
+      height: double.infinity,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: FractionalOffset(0.0, 0.6),
+          end: FractionalOffset(0.0, 1.0),
+          colors: [
+            Color.fromRGBO(52, 54, 101, 1.0),
+            Color.fromRGBO(35, 37, 57, 1.0)
+          ]
+        )
+      ),
+    );
+
+
+    final cajaRosa = Transform.rotate(
+      angle: -pi / 5.0,
+      child: Container(
+        height: 360.0,
+        width: 360.0,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(80.0),
+          gradient: LinearGradient(
+            colors: [
+              Color.fromRGBO(236, 98, 188, 1.0),
+              Color.fromRGBO(241, 142, 172, 1.0)
+            ]
+          )
+        ),
+      )
+    );
+    
+    return Stack(
+      children: <Widget>[
+        gradiente,
+        Positioned(
+          top: -100.0,
+          child: cajaRosa
+        )
+      ],
+    );
+
+  }
+
+    Widget _fondoApp(){
+
+    final gradiente = Container(
+      width: double.infinity,
+      height: double.infinity,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: FractionalOffset(0.0, 0.6),
+          end: FractionalOffset(0.0, 1.0),
+          colors: [
+            Color.fromRGBO(52, 54, 101, 1.0),
+            Color.fromRGBO(35, 37, 57, 1.0)
+          ]
+        )
+      ),
+    );
+
+
+    final cajaRosa = Transform.rotate(
+      angle: -pi / 5.0,
+      child: Container(
+        height: 360.0,
+        width: 360.0,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(80.0),
+          gradient: LinearGradient(
+            colors: [
+              Color.fromRGBO(236, 98, 188, 1.0),
+              Color.fromRGBO(241, 142, 172, 1.0)
+            ]
+          )
+        ),
+      )
+    );
+    
+    return Stack(
+      children: <Widget>[
+        gradiente,
+        Positioned(
+          top: -100.0,
+          child: cajaRosa
+        )
+      ],
+    );
+
   }
 
   Widget _textos(){
@@ -86,18 +188,111 @@ class ScrollPage extends StatelessWidget {
 
 
 
-  Widget _pagina2(){
-    return Stack(
+  Widget _pagina2( BuildContext context ){
+
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          _fondoApp2(),
+
+          SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                _titulos2(),
+                _botonesRedondeados2()
+              ],
+            ),
+          )
+
+        ],
+      ),
+      bottomNavigationBar: _bottomNavigationBar(context)
+    );
+  }
+
+  Widget _crearImagen() {
+      return Container(
+              width: double.infinity,
+              child: Image(
+              image: NetworkImage('https://www.hoerrschaudt.com/wp-content/uploads/2019/11/McGovern_Landing.jpg'),
+              height: 200.0,
+              fit: BoxFit.cover,
+            ),
+      );
+  }
+
+  Widget _crearTitulo(){
+   return SafeArea(
+        child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
+              child: Row(
+                children: <Widget>[
+
+                  Expanded(
+                      child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text('Lavanderia Rápida', style: estiloTitulo,),
+                        SizedBox(height: 7.0),
+                        Text('Lavamos su ropa', style: estilosubTitulo,)
+                      ],
+                      ),
+                  ),
+
+                    Icon(Icons.star, color: Colors.red, size: 30.0),
+                    Text('41', style: TextStyle( fontSize: 20.0 ))
+
+                ],
+                ),
+            ),
+   );
+  }
+
+  Widget _crearAcciones(){
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: <Widget>[
-        _botonBienvenido(),
-        _textos2(),
+
+        _accion( Icons.call, 'CALL' ),
+        _accion( Icons.near_me, 'ROUTE' ),
+        _accion( Icons.share, 'Share' ),
+
+        
+
       ],
     );
+  }
+
+  Widget _accion(IconData icon, String texto) {
+
+    return Column(
+          children: <Widget>[
+            Icon( icon, color: Colors.blue, size: 40.0, ),
+            SizedBox(height: 5.0,),
+            Text( texto, style: TextStyle( fontSize: 15.0, color: Colors.blue ), )
+          ],
+        );
+
+  }
+
+
+  Widget _crearTexto() {
+    return SafeArea(
+        child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 40.0),
+        child: Text(
+          'Dirección: Jr. Jorge Chavez 1154 - Breña \n\nTeléfono: 966167314', 
+          textAlign: TextAlign.justify,
+          style: TextStyle(fontSize: 20.0, color: Colors.blue),
+        ),
+      ),
+    );
+  }
     
   }
 
 
-  Widget _botonBienvenido(){
+  /*Widget _botonBienvenido(){
     return Container(
       width: double.infinity,
       height: double.infinity,
@@ -121,12 +316,13 @@ class ScrollPage extends StatelessWidget {
       
     );
   }
+  */
 
   Widget _pagina3(BuildContext context){
     return Scaffold(
       body: Stack(
         children: <Widget>[
-          _fondoApp(),
+          _fondoApp2(),
 
           SingleChildScrollView(
             child: Column(
@@ -144,7 +340,7 @@ class ScrollPage extends StatelessWidget {
   }
 
 
-  Widget _fondoApp(){
+    Widget _fondoApp2(){
 
     final gradiente = Container(
       width: double.infinity,
@@ -209,6 +405,24 @@ class ScrollPage extends StatelessWidget {
 
   }
 
+    Widget _titulos2() {
+
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text('Lavanderia Rápida', style: TextStyle( color: Colors.white, fontSize: 30.0, fontWeight: FontWeight.bold )),
+            SizedBox( height: 10.0 ),
+            Text('Información', style: TextStyle( color: Colors.white, fontSize: 18.0 )),
+          ],
+        ),
+      ),
+    );
+
+  }
+
   Widget _bottomNavigationBar(BuildContext context) {
 
     return Theme(
@@ -239,6 +453,28 @@ class ScrollPage extends StatelessWidget {
 
   }
 
+  Widget _botonesRedondeados2() {
+
+    return Table(
+      children: [
+        TableRow(
+          children: [
+            _crearBotonRedondeado( Colors.blue, FontAwesomeIcons.gamepad, 'Dirección: Jr. Jorge Chavez 1154' ),
+
+          ]
+        ),
+        TableRow(
+          children: [
+            _crearBotonRedondeado( Colors.pinkAccent, Icons.business_center, 'Teléfono: 9966167314' ),
+          ]
+        ),
+      ],
+    );
+
+    
+
+  }
+
 
   Widget _botonesRedondeados() {
 
@@ -262,14 +498,10 @@ class ScrollPage extends StatelessWidget {
             _crearBotonRedondeado( Colors.green, Icons.pets, 'Zapatillas' ),
           ]
         ),
-        TableRow(
-          children: [
-            _crearBotonRedondeado( Colors.red, Icons.rotate_right, 'Teñidos' ),
-            _crearBotonRedondeado( Colors.teal, Icons.help_outline, 'Otros' ),
-          ]
-        )
       ],
     );
+
+    
 
   }
 
@@ -304,7 +536,7 @@ class ScrollPage extends StatelessWidget {
       ),
     );
   }
-  }
+  
 
 
 
